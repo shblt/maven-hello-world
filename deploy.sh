f4693c157a0cd3479d46d081078f144760cf5569
shorbelt <shorbelt@clubos.com>
2017-01-18 17:44:46 -0500

    deploy version check

--- deploy.sh
+++ deploy.sh
@@ -5,10 +5,11 @@ echo "TRAVIS_BRANCH=$TRAVIS_BRANCH, PR=$PR, BRANCH=$BRANCH, TRAVIS_PULL_REQUEST=
 echo "TRAVIS_COMMIT=$TRAVIS_COMMIT"
 echo "TRAVIS_COMMIT_MSG=$TRAVIS_COMMIT_MSG"
 
-# ^Merge pull request .* from .*\/release-(v[\d.-]+$)
-
-if [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ "$TRAVIS_BRANCH" != "master" ] && [[ "TRAVIS_PULL_REQUEST_BRANCH" =~ release-v[\d.-]+$ ]]; then
+if [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ "$TRAVIS_BRANCH" != "master" ] && [[ "TRAVIS_COMMIT_MSG" =~ ^Merge pull request .* from .*\/release-(v[\d.-]+$) ]]; then
   echo "merge detected"
+  egrep -o ".*\/release-(v[\d.-]+$)"
+  $VERSION=$(egrep -o ".*\/release-(v[\d.-]+$)" <<<"$VERSION")
+  echo "VERSION=$VERSION"
   mvn -B release:prepare && mvn release:perform
 fi
 
