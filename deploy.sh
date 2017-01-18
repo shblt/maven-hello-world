fc91a1d42a331c8bde9eb7439bdc3ad57a960955
shorbelt <stephen.horbelt@gmail.com>
2017-01-18 17:04:41 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,10 @@
 #!/bin/bash -x
 
 BRANCH=$(if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then echo $TRAVIS_BRANCH; else echo $TRAVIS_PULL_REQUEST_BRANCH; fi)
-echo "TRAVIS_BRANCH=$TRAVIS_BRANCH, PR=$PR, BRANCH=$BRANCH"
+echo "TRAVIS_BRANCH=$TRAVIS_BRANCH, PR=$PR, BRANCH=$BRANCH, TRAVIS_PULL_REQUEST=$TRAVIS_PULL_REQUEST"
+echo "TRAVIS_COMMIT=$TRAVIS_COMMIT"
 
-echo $TRAVIS_PULL_REQUEST_BRANCH
+# ^Merge pull request .* from .*\/release-(v[\d.-]+$)
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ "$TRAVIS_BRANCH" != "master" ] && [[ "TRAVIS_PULL_REQUEST_BRANCH" =~ release-v[\d.-]+$ ]]; then
   echo "merge detected"
