fc175ee382a0310930420aead9a013399254a141
shorbelt <shorbelt@clubos.com>
2017-01-19 14:49:38 -0500

    testing

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,6 @@ echo "TRAVIS_BRANCH=$TRAVIS_BRANCH, PR=$PR, BRANCH=$BRANCH, TRAVIS_PULL_REQUEST=
 echo "TRAVIS_COMMIT=$TRAVIS_COMMIT"
 echo "TRAVIS_COMMIT_MSG=$TRAVIS_COMMIT_MSG"
 
-
 RELEASE_VERSION=$(echo $TRAVIS_COMMIT_MSG | grep -P '^Merge\spull\srequest\s.*\sfrom\s.*\/release-(v[\d.-]+$)' | grep -P -o 'release-(v[\d.-]+$)')
 echo $RELEASE_VERSION
 
